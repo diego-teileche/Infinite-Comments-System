aecd00c5f0fd639ffe1a73bf2e4b12970b74633d
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-13 22:11:11 -0300

    Project Finished
    - Inputs, buttons and comments added\n- All logics added\n- Styles updated\n- Hook created\n- Images deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react"
 import Comments from "./components/Comments"
 
 import "./App.css"
+import useNode from "./hooks/useNode"
+
+const comment = {
+	id: 1,
+	items: [],
+}
 
 function App() {
+	const [commentsData, setCommentsData] = useState(comment)
+
+	const { insertNode, editNode, deleteNode } = useNode()
+
+	const handleInsertNode = (folderId, item) => {
+		const finalStructure = insertNode(commentsData, folderId, item)
+		setCommentsData(finalStructure)
+	}
+
+	const handleEditNode = (folderId, value) => {
+		const finalStructure = editNode(commentsData, folderId, value)
+		setCommentsData(finalStructure)
+	}
+
+	const handleDeleteNode = (folderId) => {
+		const finalStructure = deleteNode(commentsData, folderId)
+		const temp = { ...finalStructure }
+		setCommentsData(temp)
+	}
+
 	return (
 		<div className="App">
-			<Comments />
+			<Comments
+				handleInsertNode={handleInsertNode}
+				handleEditNode={handleEditNode}
+				handleDeleteNode={handleDeleteNode}
+				comment={commentsData}
+			/>
 		</div>
 	)
 }
